2992c74c6061f81bc30a6082016b91ed1cb59a08
Cody Lacey <cody605@gmail.com>
2021-08-30 20:09:44 -0500

    first

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+
+
+
+
+// async function deletePokemon(){
+//     try{
+//         const res = await fetch('/grabRecipe', {
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//                 'recipeName': rName,
+//                 'recipeDescription': description
+//             })
+//         })
+//         const data = await res.json()
+//         console.log(data);
+        
+        
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+const recipeScraper = require('recipe-scraper')
+
+// Mongo
+let db,
+    dbConnectionStr = 'mongodb+srv://db1:1234@cluster0.vjerq.mongodb.net/todo?retryWrites=true&w=majority',
+    dbName = 'GroceryList'
+
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`);
+        db = client.db(dbName)
+    })
+
+//
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    try{
+    res.render('index.ejs')
+    }catch {
+        console.log(err);
+    }
+})
+
+app.post('/grabRecipe', (req, res) => {
+    let url = req.body.url
+
+    recipeScraper(url)
+    .then(data => {
+        res.render('recipe.ejs', {recipe: data})
+    })
+
+
+    // async function grabUrl() {
+    //     let data = await recipeScraper(url)
+        
+    // }
+    // grabUrl()
+})
+
+
+
+
+    app.listen(process.env.PORT || PORT, () => {
+    console.log(`server running on ${PORT}`);
+})
+
