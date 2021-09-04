ad8b24d369d2178ee29581235241ec5cce0484f4
Cody Lacey <cody605@gmail.com>
2021-09-04 13:55:40 -0500

    update

--- public/js/main.js
+++ public/js/main.js
@@ -8,22 +8,31 @@ document.querySelector('.print').addEventListener('click', printRecipe)
 
 function printRecipe(){
     document.title = document.querySelector('.recipeName').innerHTML
+
     document.querySelector(".recipeSaveLink").style.display = "none"
     document.querySelector(".print").style.display = "none"
-    document.querySelector(".recipeStuff").style.display = "none"
+    document.querySelector(".recipeImage").style.display = "none"
+    // document.querySelector(".recipeImage").style.visibility = "hidden"
+    // document.querySelector(".recipeSaveLink").style.visibility = "hidden"
+    // document.querySelector(".print").style.visibility = "hidden"
 
 	let printContents = document.getElementById('recipe').innerHTML;
 	let originalContents = document.body.innerHTML;
 
 	document.body.innerHTML = printContents;
 
+    
 	window.print();
-
 	document.body.innerHTML = originalContents;
 
-    document.querySelector(".recipeStuff").style.display = "inline-Block"
-    document.querySelector(".print").style.display = "inline"
+    // document.querySelector(".recipeImage").style.visibility = "visible"
+    // document.querySelector(".recipeSaveLink").style.visibility = "visible"
+    // document.querySelector(".print").style.visibility = "visible"
+    // document.querySelector(".recipeStuff").style.display = "inline-Block"
+    document.querySelector('.recipePt2').style.visibility = 'visible'
+    document.querySelector(".print").style.display = "initial"
     document.querySelector(".recipeSaveLink").style.display = "inline-block"
+    document.querySelector(".recipeImage").style.display = "block"
 
 }
 // async function deletePokemon(){
