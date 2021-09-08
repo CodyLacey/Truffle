2039e855ecf8c55f9cbc9edd6da250e4818e8135
Cody Lacey <cody605@gmail.com>
2021-09-07 22:44:06 -0500

    updated links and printing

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,37 @@
-document.querySelector('.top').addEventListener('click', home)
 
-function home() {
-    location.href = "/"
+//store original recipe
+document.querySelector('.importRecipe2').addEventListener('click', store)
+function store() { 
+    let url = document.getElementById('input')
+    localStorage.setItem('url', url.value)
 }
+document.getElementById('anchor').setAttribute('href', localStorage.getItem('url'))
 
+
+
+
+
+//Print
 document.querySelector('.print').addEventListener('click', printRecipe)
-
 function printRecipe(){
     document.title = document.querySelector('.recipeName').innerHTML
 
     document.querySelector(".recipeSaveLink").style.display = "none"
     document.querySelector(".print").style.display = "none"
     document.querySelector(".recipeImage").style.display = "none"
-    // document.querySelector(".recipeImage").style.visibility = "hidden"
-    // document.querySelector(".recipeSaveLink").style.visibility = "hidden"
-    // document.querySelector(".print").style.visibility = "hidden"
 
 	let printContents = document.getElementById('recipe').innerHTML;
 	let originalContents = document.body.innerHTML;
 
 	document.body.innerHTML = printContents;
 
-    
 	window.print();
+
 	document.body.innerHTML = originalContents;
 
-    // document.querySelector(".recipeImage").style.visibility = "visible"
-    // document.querySelector(".recipeSaveLink").style.visibility = "visible"
-    // document.querySelector(".print").style.visibility = "visible"
-    // document.querySelector(".recipeStuff").style.display = "inline-Block"
     document.querySelector('.recipePt2').style.visibility = 'visible'
     document.querySelector(".print").style.display = "initial"
     document.querySelector(".recipeSaveLink").style.display = "inline-block"
     document.querySelector(".recipeImage").style.display = "block"
 
 }
-// async function deletePokemon(){
-//     try{
-//         const res = await fetch('/grabRecipe', {
-//             method: 'POST',
-//             headers: {'Content-Type': 'application/json'},
-//             body: JSON.stringify({
-//                 'recipeName': rName,
-//                 'recipeDescription': description
-//             })
-//         })
-//         const data = await res.json()
-//         console.log(data);
-        
-        
-//     }catch(err){
-//         console.log(err);
-//     }
-// }
